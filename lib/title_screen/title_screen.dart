c4f630b58fd87f277297682a822489e722de5e91
Soroush Beigi <sbeygi712@gmail.com>
2024-02-16 21:49:41 +0330

    Using the lit color class

--- lib/title_screen/title_screen.dart
+++ lib/title_screen/title_screen.dart
@@ -1,25 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_next_gen_ui/styles.dart';
+import 'package:flutter_next_gen_ui/title_screen/lit_image.dart';
 
 import '../assets.dart';
 
 class TitleScreen extends StatelessWidget {
   const TitleScreen({super.key});
 
+  final _emitAmt = 0.7;
+  final _orbaMT = 0.5;
+
   @override
   Widget build(BuildContext context) {
+    final orbColor = AppColors.orbColors[0];
+    final emitColor = AppColors.emitColors[0];
+
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
         child: Stack(
           children: [
             Image.asset(AssetPaths.titleBgBase),
-            Image.asset(AssetPaths.titleBgReceive),
-            Image.asset(AssetPaths.titleMgBase),
-            Image.asset(AssetPaths.titleMgReceive),
-            Image.asset(AssetPaths.titleMgEmit),
-            Image.asset(AssetPaths.titleFgBase),
-            Image.asset(AssetPaths.titleFgReceive),
-            Image.asset(AssetPaths.titleFgEmit),
+            LitImage(
+              color: orbColor,
+              imgSrc: AssetPaths.titleBgReceive,
+              lightAmt: _orbaMT,
+            ),
+            LitImage(
+              color: orbColor,
+              imgSrc: AssetPaths.titleMgBase,
+              lightAmt: _orbaMT,
+            ),
+            LitImage(
+              color: orbColor,
+              imgSrc: AssetPaths.titleMgReceive,
+              lightAmt: _orbaMT,
+            ),
+            LitImage(
+                color: emitColor,
+                imgSrc: AssetPaths.titleMgEmit,
+                lightAmt: _emitAmt),
+            Image.asset(
+              AssetPaths.titleFgBase,
+            ),
+            LitImage(
+              color: orbColor,
+              imgSrc: AssetPaths.titleFgReceive,
+              lightAmt: _orbaMT,
+            ),
+            LitImage(
+              color: emitColor,
+              imgSrc: AssetPaths.titleFgEmit,
+              lightAmt: _emitAmt,
+            ),
           ],
         ),
       ),
